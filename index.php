e2142a16c52f8c140c5a544294f125afdf75a67e
yuanguang.wang <yuanguang.m.wang@outlook.com>
2022-08-17 01:19:07 +0800

    T.20220817.0118 redo

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>elderaven</title>
+</head>
+
 <body>
-<p> test</p>
-<?php
-echo "hello world";
 
-$testNum = 1.43;
-print $testNum;
-?>
-
-<p> vscode test </p>
-<p> test 2 </p>
-<p> test 3 </p>
-<?php
-print "test4";
-?>
 </body>
-</html>
 
+</html>

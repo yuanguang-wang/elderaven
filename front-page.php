011fb8fd0a6ef75919aba14ef37ae7128cf54685
yuanguang.wang <yuanguang.m.wang@outlook.com>
2022-08-17 13:49:38 +0800

    T.20220817.1348 Mark as wp skeleton set up finished

--- front-page.php
+++ front-page.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php get_header(); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>elderaven</title>
-
-    <!-- Load CSS -->
-    <?php
-    wp_head();
-    ?>
-
-</head>
-
-<body>
-
-</body>
-
-</html>
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php
 
+function rvn_theme_support()
+{
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'rvn_theme_support');
+
 function rvn_load_styles()
 {
     $version = wp_get_theme()->get("Version");
@@ -12,5 +19,11 @@ add_action('wp_enqueue_scripts', 'rvn_load_styles');
 
 function rvn_load_scripts()
 {
-    
+    $version = wp_get_theme()->get("Version");
+    wp_enqueue_script('jquery_script', "https://code.jquery.com/jquery-3.6.0.slim.min.js", array(), '3.6.0', true);
+    wp_enqueue_script('popper_script', "https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js", array(), '2.9.2', true);
+    wp_enqueue_script('bootstrap_script', "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js", array(), '5.2.0', true);
+    wp_enqueue_script('main_script', get_template_directory_uri() . "/assets/js/main.js", array(), $version, true);
 }
+
+add_action('wp_enqueue_scripts', 'rvn_load_scripts');

14059052dd8974fbaf355cc393da5e0de261b36e
yuanguang.wang <yuanguang.m.wang@outlook.com>
2022-08-20 10:38:44 +0800

    T.20220820.1038 finish gallery index

---
+++ page-gallery.php
@@ -0,0 +1,81 @@
+<?php get_header(); ?>
+<div class="container">
+
+    <hr>
+    <div class="m-2 text-center">
+        <h4>Project Gallery</h4>
+    </div>
+    <hr>
+
+    <div class="row row-cols-md-4 g-4 pt-2">
+        <div class="col-sm">
+            <div class="card">
+                <a href="https://elderaven.com/15-minus-and-counting/">
+                    <img src="/wp-content/themes/elderaven/assets/images/gallerycard/01.jpg" alt="15 Minutes and Counting" class="card-img-top">
+                </a>
+                <div class="card-footer">15 Minutes and Counting</div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card">
+                <img src="/wp-content/themes/elderaven/assets/images/gallerycard/01.jpg" alt="15 Minutes and Counting" class="card-img-top">
+                <div class="card-footer">16 Minutes and Counting</div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card">
+                <img src="/wp-content/themes/elderaven/assets/images/gallerycard/01.jpg" alt="15 Minutes and Counting" class="card-img-top">
+                <div class="card-footer">17 Minutes and Counting</div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card">
+                <img src="/wp-content/themes/elderaven/assets/images/gallerycard/01.jpg" alt="15 Minutes and Counting" class="card-img-top">
+                <div class="card-footer">18 Minutes and Counting</div>
+            </div>
+        </div>
+
+    </div>
+    <div class="row row-cols-md-4 g-4 pt-5">
+        <div class="col-sm">
+            <div class="card bg-light">
+                <img src="/wp-content/themes/elderaven/assets/images/gallerycard/01.jpg" alt="15 Minutes and Counting" class="card-img-top">
+                <div class="card-body">
+                    <p class="card-text">15 Minutes and Counting</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card bg-light">
+                <img src="/wp-content/themes/elderaven/assets/images/gallerycard/01.jpg" alt="15 Minutes and Counting" class="card-img-top">
+                <div class="card-body">
+                    <p class="card-text">15 Minutes and Counting</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card bg-light">
+                <img src="/wp-content/themes/elderaven/assets/images/gallerycard/01.jpg" alt="15 Minutes and Counting" class="card-img-top">
+                <div class="card-body">
+                    <p class="card-text">15 Minutes and Counting</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card bg-light">
+                <img src="/wp-content/themes/elderaven/assets/images/gallerycard/01.jpg" alt="15 Minutes and Counting" class="card-img-top">
+                <div class="card-body">
+                    <p class="card-text">15 Minutes and Counting</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+<?php get_footer(); ?>
+</div>
